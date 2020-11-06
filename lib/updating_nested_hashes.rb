598b4bb0533cc3aae7c8749b9f8195cdefcb77ff
Irvin Natt <ipnatt@gmail.com>
2020-11-06 02:40:59 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -1,5 +1,5 @@
 def update_status
-   epic_tragedy[:montague][:hero][:status] = "dead"
+   epic_tragedy[:montague][:hero][:status]
       :montague => {
          :patriarch => {name: "Lord Montague", age: "53"},
          :matriarch => {name: "Lady Montague", age: "54"},
